fff68ada1002618440671d84bb6a520a13fc202d
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-06-22 19:32:36 +0600

    update db name and playground deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,8 +11,12 @@ require('dotenv').config();
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      introspection: true,
+      playground: true,
     }),
-    MongooseModule.forRoot(process.env.MONGO_URL),
+    MongooseModule.forRoot(process.env.MONGO_URL, {
+      dbName: process.env.MONGO_DB_NAME,
+    }),
     UserModule,
   ],
 })
